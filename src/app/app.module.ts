c5ec79c5c795c7f2d16ed384cc9e5ea5fe956a21
berberAlla <alysik19842807@gmail.com>
2019-10-24 11:27:50 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './auth/login/login.component';
 import {SharedModule} from './shared/shared.module';
-import { SignupComponent } from './auth/signup/signup.component';
 import {RouterModule} from '@angular/router';
 import {AppRoutingModule} from './app-routing.module';
 
